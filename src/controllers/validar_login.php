b3cb1c5f1c6ee442b2f22b83a2196a87750150dc
Eduardo Gustavo <66931597+dudugudu@users.noreply.github.com>
2024-11-23 17:27:58 -0300

    Fix validação ajuste notify

--- src/controllers/validar_login.php
+++ src/controllers/validar_login.php
@@ -8,31 +8,32 @@ session_start(); // Inicia a sessão
 require_once __DIR__ . '/../../config.php'; // Inclui o arquivo de configuração
 
 require_once (SRC . 'Controllers/User/ControllerUser.php'); // Inclui o controlador de usuário
-
 use Controllers\ControllerUser;
 
-if(isset($_POST['user']) && isset($_POST['senha'])){
+require_once (SRC . 'utils/notify.php'); // Inclui o arquivo de utilidades
+use Utils\Notify;
+$notify = new Notify(); // Instancia a classe de notificação
+
+
+if (!empty($_POST['user']) && !empty($_POST['senha'])) {
     // Verifica se os campos 'user' e 'senha' foram enviados via POST
-    $user = $_POST['user'];
+    $username = $_POST['user'];
     $senha = $_POST['senha'];
 
     $controller = new ControllerUser();
-    $user = $controller->validarLogin($user, $senha); // Valida o login do usuário
-    if($user){
+    $user = $controller->validarLogin($username, $senha); // Valida o login do usuário
+    if($user->status){
         // Se o login for bem-sucedido
-        echo "Logado com sucesso!";
         $_SESSION['user'] = serialize($user); // Armazena o objeto usuário na sessão
         header('Location: '. VIEWS_URL . 'dashboard.php'); // Redireciona para o dashboard
     }else{
         // Se o login falhar
-        echo "Usuário ou senha inválidos, tente novamente!";
-        $_SESSION['erroLogin'] = "Usuário ou senha inválidos, tente novamente!"; // Armazena mensagem de erro na sessão
+        $notify->addMessage('Usuário ou senha inválidos!', 'error'); // Adiciona uma mensagem de erro
         header('Location: '.ROOT_URL.'index.php'); // Redireciona para a página inicial
     }
 }else{
     // Caso os dados não tenham sido enviados via POST
-
-    $_SESSION['erroLogin'] = "Você precisa estar logado para acessar esta página!"; // Armazena mensagem de erro na sessão
+    $notify->addMessage('Ensira o usuário e a senha!', 'error'); // Adiciona uma mensagem de erro
     header('Location: '.ROOT_URL.'index.php'); // Redireciona para a página inicial
 }
 ?>
